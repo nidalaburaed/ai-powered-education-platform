2bc75f3aabd387581115070fc2f3b707d0db0bff
nidalaburaed <nidalaburaed@hotmail.com>
2026-08-26 19:02:51 +0300

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -21,7 +21,7 @@ if "localhost" not in frontend_url:
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=allow_origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
